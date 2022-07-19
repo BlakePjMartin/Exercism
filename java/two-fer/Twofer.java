2443495011aff0a606b6b6b51c614d2146132d80
Blake Martin <76712149+BlakePjMartin@users.noreply.github.com>
2022-07-19 09:18:29 -0300

    Delete java/two-fer directory

--- java/two-fer/Twofer.java
+++
@@ -1,6 +0,0 @@
-public class Twofer {
-    public String twofer(String name) {
-        String finalStr = (name == null) ? "you" : name;
-        return "One for " + finalStr + ", one for me.";
-    }
-}
